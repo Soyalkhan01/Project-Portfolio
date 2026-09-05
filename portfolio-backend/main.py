a4f1c14b375fd77004825f691420739bce0464dc
shaheerkhan <its.shaheerkhan728@gmail.com>
2026-09-05 13:16:54 +0530

    Configure production CORS

--- portfolio-backend/main.py
+++ portfolio-backend/main.py
@@ -64,7 +64,7 @@ app.add_exception_handler(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://portfolio-frontend-beta-eight.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
